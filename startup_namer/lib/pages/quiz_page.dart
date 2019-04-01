529b092c5f69f9849cbc9c659969ffea3e806cda
qbylucky <18600211621@163.com>
2019-04-02 00:14:23 +0800

    update

---
+++ startup_namer/lib/pages/quiz_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../utils/question.dart';
+import '../utils/quiz.dart';
+
+class QuizPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new QuizPageState();
+}
+
+class QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: <Widget>[],
+    );
+  }
+}
