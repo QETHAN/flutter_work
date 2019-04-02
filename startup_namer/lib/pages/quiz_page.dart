814e7111970d033ba0086b5e10f9f06f4e13693c
binyang qin <qinbinyang@hupu.com>
2019-04-02 23:53:58 +0800

    update

--- startup_namer/lib/pages/quiz_page.dart
+++ startup_namer/lib/pages/quiz_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 import '../utils/question.dart';
 import '../utils/quiz.dart';
+import '../UI/answer_button.dart';
+import '../UI/question_text.dart';
+import '../UI/correct_wrong_overlay.dart';
 
 class QuizPage extends StatefulWidget {
   @override
@@ -9,10 +12,63 @@ class QuizPage extends StatefulWidget {
 }
 
 class QuizPageState extends State<QuizPage> {
+  Question currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question("Elon Mask is Human", false),
+    new Question('Pizza is heathy', true),
+    new Question('Flutter is awesome', true)
+  ]);
+
+  String questionText;
+  int questionNumber;
+  bool isCorrect;
+  bool overlayShouldBeVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    currentQuestion = quiz.nextQuestion;
+    questionText = currentQuestion.question;
+    questionNumber = quiz.questionNumber;
+  }
+
+  void handleAnswer(bool answer) {
+    isCorrect = currentQuestion.answer == answer;
+    quiz.answer(isCorrect);
+    this.setState(() {
+      overlayShouldBeVisible = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Stack(
-      children: <Widget>[],
+      fit: StackFit.expand,
+      children: <Widget>[
+        new Column(
+          children: <Widget>[
+            new AnswerButton(true, () => handleAnswer(true)),
+            new QuestionText(questionText, questionNumber),
+            new AnswerButton(false, () => handleAnswer(false))
+          ],
+        ),
+        overlayShouldBeVisible == true
+            ? new CorrectWrongOverlay(isCorrect, () {
+                currentQuestion = quiz.nextQuestion;
+                if (currentQuestion == null) {
+                  this.setState(() {
+                    overlayShouldBeVisible = false;
+                  });
+                } else {
+                  this.setState(() {
+                    overlayShouldBeVisible = false;
+                    questionText = currentQuestion.question;
+                    questionNumber = quiz.questionNumber;
+                  });
+                }
+              })
+            : new Container()
+      ],
     );
   }
 }
