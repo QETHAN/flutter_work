753e4e4c592e808ed71e47cef31325d0f1ec309f
binyang qin <qinbinyang@hupu.com>
2019-04-28 17:47:49 +0800

    update

--- ui_demo/lib/layout.dart
+++ ui_demo/lib/layout.dart
@@ -45,51 +45,60 @@ class LayoutPage extends StatelessWidget {
     );
 
     // 图文
-    Widget cardItem = new Container(
-      decoration: BoxDecoration(color: Colors.blue[50]),
-      margin: EdgeInsets.only(top: 30),
-      padding: EdgeInsets.all(15),
-      height: 130,
-      child: Row(
-        // mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Image.network(
-              'http://a.hiphotos.baidu.com/image/h%3D300/sign=45e16856990a304e4d22a6fae1c9a7c3/ac4bd11373f08202e2518d6d45fbfbedaa641ba4.jpg',
-              fit: BoxFit.cover,
-              width: 100,
-              height: 100,
-            ),
+    Widget cardItem = Stack(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(color: Colors.blue[50]),
+          margin: EdgeInsets.only(top: 30),
+          padding: EdgeInsets.all(15),
+          height: 130,
+          child: Row(
+            // mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  'http://a.hiphotos.baidu.com/image/h%3D300/sign=45e16856990a304e4d22a6fae1c9a7c3/ac4bd11373f08202e2518d6d45fbfbedaa641ba4.jpg',
+                  fit: BoxFit.cover,
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                      // decoration: BoxDecoration(color: Colors.grey),
+                      margin: EdgeInsets.only(left: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Text('这是一个标题这是一个标题这是一个标题',
+                              maxLines: 2,
+                              softWrap: true,
+                              overflow: TextOverflow.ellipsis),
+                          Container(
+                            margin: EdgeInsets.only(top: 10),
+                            padding: EdgeInsets.symmetric(vertical: 1, horizontal: 3),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Colors.red, width: 1),
+                                borderRadius: BorderRadius.circular(4)),
+                            child: Text('标签1', style: TextStyle(fontSize: 10),),
+                          )
+                        ],
+                      )))
+            ],
           ),
-          Expanded(
-              flex: 1,
-              child: Container(
-                  // decoration: BoxDecoration(color: Colors.grey),
-                  margin: EdgeInsets.only(left: 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Text('这是一个标题这是一个标题这是一个标题',
-                          maxLines: 2,
-                          softWrap: true,
-                          overflow: TextOverflow.ellipsis),
-                      Container(
-                        margin: EdgeInsets.only(top: 10),
-                        padding: EdgeInsets.symmetric(vertical: 1, horizontal: 3),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Colors.red, width: 1),
-                            borderRadius: BorderRadius.circular(4)),
-                        child: Text('标签1', style: TextStyle(fontSize: 10),),
-                      )
-                    ],
-                  )))
-        ],
-      ),
+        ),
+        Positioned(
+          right: 10,
+          bottom: 10,
+          child: Text('右下角定位'),
+        )
+      ]
     );
 
     return new MaterialApp(
